e57e63306f34d04f76ba2227bf93f9dbbb2936b5
unknown <dwiprajdey33720@gmail.com>
2025-10-25 19:16:17 +0530

    fixing resume

--- myportfolio/src/components/About/About.jsx
+++ myportfolio/src/components/About/About.jsx
@@ -57,7 +57,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="resume"
+            href="https://drive.google.com/file/d/1YVmzgCnER8HC5hTRKobUKL-jmZj5lEij/view?usp=drivesdk "
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
